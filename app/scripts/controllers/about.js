ec930240ff6a623a99a17a6e1773d8789e255af7
Anubhav Vishwakarma <anuvishw@publicisgroupe.net>
2016-05-09 18:57:08 +0530

    Initialising chatU

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatApplicationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatApplicationApp
+ */
+angular.module('chatApplicationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
